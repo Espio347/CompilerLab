29782a5d7d805dd0ade02d0a83acb28d738a0876
Naman Kishwan <92114546+Espio347@users.noreply.github.com>
2023-06-16 07:10:39 +0530

    Update and rename regex_to_DFA.c to regex_to_NFA.c

--- regex_to_NFA.c
+++ regex_to_NFA.c
@@ -15,6 +15,9 @@ bool matchesPattern(char* str) {
         if (state == 0 && str[i] == 'a') {
             state = 1;
             printf("State 0, Input 'a' -> State 1\n");
+        } else if (state == 1 && str[i] == 'a') {
+            state = 1;
+            printf("State 1, Input 'a' -> State 1\n");
         } else if (state == 1 && str[i] == 'b') {
             state = 2;
             printf("State 1, Input 'b' -> State 2\n");
@@ -38,29 +41,6 @@ bool matchesPattern(char* str) {
         printf("Error! No path with current symbol to Final State.\n");
         return false;
     }
-
-while (str[i] != '\0') {
-        if (state == 0 && str[i] == 'a') {
-            state = 1;
-        } else if (state == 1 && str[i] == 'b') {
-            state = 2;
-        } else if (state == 2 && str[i] == 'a') {
-            state = 1;
-        } else if (state == 2 && str[i] == 'b') {
-            state = 2;
-        } else {
-            return false;
-        }
-        i++;
-    }
-
-    // Check if the final state is reached
-    if (state == 1 || state == 2) {
-        return true;
-    } else {
-        return false;
-    }
-
 }
 
 int main() {
@@ -72,4 +52,3 @@ int main() {
     }
     return 0;
 }
-
